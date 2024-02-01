5dfa6d0485d7d3a3663570f608001631533ba109
subodh4003 <158164957+subodh4003@users.noreply.github.com>
2024-02-01 11:28:56 +0530

    chage nav name

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -15,13 +15,13 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse " id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li className="nav-item ">
-          <a className="nav-link active bdr" aria-current="page" href="/">Home1</a>
+          <a className="nav-link active bdr" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link bdr" href="/">About1</a>
+          <a className="nav-link bdr" href="/">About</a>
         </li>
           <li className="nav-item">
-            <a href="/" className="nav-link bdr">Contact1</a>
+            <a href="/" className="nav-link bdr">Contact</a>
           </li>
 
       </ul>
